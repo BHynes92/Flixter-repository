c905405077860bf3db8dd5efbed19e90cae61d1b
Brendan Hynes <BHynes92@gmail.com>
2019-10-21 22:46:44 +0000

    Flixter Lesson 24

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,18 @@ CarrierWave.configure do |config|
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public_read"
 
+  if Rails.env.development?
+    config.cache_dir='/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+  
   config.aws_credentials = {
     access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     secret_access_key: ENV["AWS_SECRET_KEY"],        # required
     region:            ENV["AWS_REGION"]
   }
 end
+
 
 #This is the old version of the carrierwave.rb file which was copy/pasted from the Lesson documentation
 # config.fog_provider = 'fog/aws'                        # required
